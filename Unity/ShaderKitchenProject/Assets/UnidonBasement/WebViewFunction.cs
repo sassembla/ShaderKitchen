70d7e1bf26ad076cbb5e94bfcbf528486cbbad01
Toru <toru.inoue@kissaki.tv>
2016-03-26 15:25:36 +0900

    じわじわ設定中。

--- Unity/ShaderKitchenProject/Assets/UnidonBasement/WebViewFunction.cs
+++ Unity/ShaderKitchenProject/Assets/UnidonBasement/WebViewFunction.cs
@@ -4,79 +4,21 @@ using UnityEngine;
 public class WebViewFunction {
 	private static Markdown markdownSharp = new Markdown();
 	
-	public static string MarkdownToHTML (string markdown) {
-		return markdownSharp.Transform(markdown);
-	}
-	private static bool first = true;
-	private static Color col;
-	
-	public static void DrawHTML (string text, Rect showRect) {
-		// この関数の中身を、ブラウザで見ているときとそうでないときで入れ替えればいい。
+	public static string MarkdownToRichText (string markdown) {
+		var html = markdownSharp.Transform(markdown);
 		
-		/*
-			stack color, style, cursors.
-		*/
-		var backupColor = GUI.color;
-		var backupContentColor = GUI.contentColor;
-		var backupBackgroundColor = GUI.backgroundColor;
-
-		//add textarea with transparent text
-		GUI.contentColor = new Color(1f, 1f, 1f, 0f);
-		GUIStyle style = new GUIStyle(GUI.skin.textArea);
-		text = GUI.TextArea(showRect, text);
-
+		var richtext = HTMLToRichText(html);
 		
-		int controlID = GUIUtility.GetControlID(showRect.GetHashCode(), FocusType.Keyboard);    
-		TextEditor editor = (TextEditor)GUIUtility.GetStateObject(typeof(TextEditor), controlID -1);
-
-		//set background of all textfield transparent
-		    
-
-		//backup selection to remake it after process
-		int backupPos = editor.cursorIndex;
-		int backupSelPos = editor.selectIndex;
-		
-		
-		//get last position in text
-		editor.MoveTextEnd();
-		int endpos = editor.cursorIndex;
-		
-		GUI.backgroundColor = new Color(1f, 1f, 1f, 0f);
-
-		//set word color
-		if (first) {
-			col = new Color(UnityEngine.Random.Range(0f, 1f), UnityEngine.Random.Range(0f, 1f), UnityEngine.Random.Range(0f, 1f));
-			first = false;
-		}
-		GUI.contentColor = col;
-
-		//draw textfield with color on top of text area
-		editor.MoveTextStart();        
-		while (editor.cursorIndex != endpos) {
-			editor.SelectToStartOfNextWord();
-			string wordtext = editor.SelectedText;
-			
-			//draw each word with a random color
-			Vector2 pixelselpos = style.GetCursorPixelPosition(editor.position, editor.content, editor.selectIndex);
-			Vector2 pixelpos = style.GetCursorPixelPosition(editor.position, editor.content, editor.cursorIndex);
-			GUI.TextField(new Rect(pixelselpos.x - style.border.left, pixelselpos.y - style.border.top, pixelpos.x, pixelpos.y), wordtext);
-
-			editor.MoveToStartOfNextWord();
-		}
-
-		//Reposition selection
-		Vector2 bkpixelselpos = style.GetCursorPixelPosition(editor.position, editor.content, backupSelPos);    
-		editor.MoveCursorToPosition(bkpixelselpos);    
-
-		//Remake selection
-		Vector2 bkpixelpos = style.GetCursorPixelPosition(editor.position, editor.content, backupPos);    
-		editor.SelectToPosition(bkpixelpos);    
-
-		//Reset color
-		GUI.color = backupColor;
-		GUI.contentColor = backupContentColor;
-		GUI.backgroundColor = backupBackgroundColor;
+		return richtext;
 	}
 	
-	
+	private static string HTMLToRichText (string html) {
+		var lines = html.Replace("<br>", "\n")
+			// このへんで、キーワードごとのハイライトも可能。
+			.Replace("<p>", "<color=#ff0000>").Replace("</p>", "</color>")
+			.Replace("<h2>", "<size=30>").Replace("</h2>", "</size>");
+		
+		
+		return lines;
+	}
 }
--- Unity/ShaderKitchenProject/Assets/UnidonContents/Page_1/WebTextScript.cs
+++ Unity/ShaderKitchenProject/Assets/UnidonContents/Page_1/WebTextScript.cs
@@ -3,18 +3,14 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class WebTextScript : MonoBehaviour {
-	private string html;
-	private Rect textRect;
-	
 	// Use this for initialization
 	void Start () {
+		// この部分がGUIから済むようになるといいな。設定、表示を含めて。
 		var rectTrans = GetComponent<InputField>();
 		
 		// load text data from .md
-		var bindata = Resources.Load("Page_1") as TextAsset;
-		html = WebViewFunction.MarkdownToHTML(bindata.text);
-		Debug.LogError("html:" + html);
-		rectTrans.text = html;
+		var bindata = Resources.Load("Page_1") as TextAsset;// ここ、AssetBundleにできるといいなあ。更新が楽。
+		rectTrans.text = WebViewFunction.MarkdownToRichText(bindata.text);
 	}
 	
 }
