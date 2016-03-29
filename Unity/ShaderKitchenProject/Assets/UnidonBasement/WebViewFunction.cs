9fb7226c246a6034b2ab706898ed00e6eac47d12
Toru <toru.inoue@kissaki.tv>
2016-03-30 03:00:50 +0900

    カラーリング部分、追加しやすくしてみた。

--- Unity/ShaderKitchenProject/Assets/UnidonBasement/WebViewFunction.cs
+++ Unity/ShaderKitchenProject/Assets/UnidonBasement/WebViewFunction.cs
@@ -17,37 +17,31 @@ namespace Unidon {
 		
 		private static string HTMLToRichText (string html) {
 			var lines = html.Replace("<br>", "\n")
-			// このへんで、キーワードごとのハイライトも可能。
+			
+			/*
+				基礎サイズとか
+			*/
 			.Replace("<p>", "").Replace("</p>", "")
 			.Replace("<h2>", "<size=30>").Replace("</h2>", "</size>")
 			.Replace("<pre><code class=\"language-\">", "").Replace("</code></pre>", "")
 			.Replace("/**/", "")
-
-			//追加 青
-			.Replace("Shader ","<color=#00aaff>Shader </color>")
-			.Replace("SubShader","<color=#00aaff>SubShader</color>")
-			.Replace("Properties","<color=#00aaff>Properties</color>")
-			.Replace("Tags","<color=#00aaff>Tags</color>")
-			.Replace("Pass","<color=#00aaff>Pass</color>")
-			.Replace("float4","<color=#00aaff>float4</color>")
-			.Replace("float2","<color=#00aaff>float2</color>")
-			.Replace("fixed4","<color=#00aaff>fixed4</color>")
-			.Replace("fixed2","<color=#00aaff>fixed2</color>")
-			.Replace("half4","<color=#00aaff>half4</color>")
-			.Replace("half2","<color=#00aaff>half2</color>")
-			.Replace("sampler2D","<color=#00aaff>sampler2D</color>")
-			.Replace("return","<color=#00aaff>return</color>")
-			//追加 緑
-			.Replace("struct","<color=#00ffaa>Struct </color>")
-			.Replace("Fallback","<color=#00ffaa>Fallback</color>")
-			.Replace("tex2D","<color=#00ffaa>tex2D</color>")
-			.Replace("mul","<color=#00ffaa>mul</color>")
-			.Replace("TRANSFORM_TEX","<color=#00ffaa>TRANSFORM_TEX</color>")
-			//追加 紫
-			.Replace("CGPROGRAM","<color=#ff33ff>CGPROGRAM</color>")
-			.Replace("ENDCG","<color=#ff33ff>ENDCG</color>")
-			.Replace("#pragma","<color=#ff33ff>#pragma</color>")
-			.Replace("#include","<color=#ff33ff>#include</color>")
+			
+			
+			/*
+				word tagging
+			*/
+			.SetWordWrappingTag(
+				new Dictionary<TagPair, List<string>> {
+					// blue
+					{new TagPair("<color=#00aaff>", "</color>"), new List<string>{"Shader ", "SubShader", "Properties", "Tags", "Pass", "float4", "float2", "fixed4", "fixed2", "half4", "half2", "sampler2D", "return",}},
+					
+					// green
+					{new TagPair("<color=#00ffaa>", "</color>"), new List<string>{"struct", "Fallback", "tex2D", "mul", "TRANSFORM_TEX",}},
+					
+					// purple
+					{new TagPair("<color=#ff33ff>", "</color>"), new List<string>{"CGPROGRAM", "ENDCG", "#pragma", "#include",}},
+				}
+			)
 			
 			// 囲まれてる範囲の色を変える系
 			.SetTagsOfWrappedLine(
@@ -67,6 +61,15 @@ namespace Unidon {
 			;
 
 			return lines;
+		}
+		
+		private static string SetWordWrappingTag (this string source, Dictionary<TagPair, List<string>> tagDict) {
+			foreach (var tag in tagDict.Keys) {
+				foreach (var keyword in tagDict[tag]) {
+					source = source.Replace(keyword, tag.startTag + keyword + tag.endTag);
+				}
+			}
+			return source;
 		}
 		
 		private static string SetTagsOfWrappedLine (this string source, Dictionary<char, TagPair> keyAndTag) {
